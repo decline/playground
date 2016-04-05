66c875ab993c12edebdac5ea66e1feb79d6cf46f
Dominik Huppmann <dominik.huppmann@gmail.com>
2016-04-05 18:20:54 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('concat', function () {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('./web/js'));
+});
+
+gulp.task('uglify', function () {
+    return gulp.src('./web/js/script.js')
+        .pipe(uglify())
+        .pipe(rename('script.min.js'))
+        .pipe(gulp.dest('./web/js'));
+});
+
+gulp.task('default', ['concat', 'uglify']);
