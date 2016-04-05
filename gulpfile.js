3cf81ce16bbd3d13222204257f1f8db6f049453b
Dominik Huppmann <dominik.huppmann@gmail.com>
2016-04-05 18:36:49 +0200

    cool fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 gulp.task('concat', function () {
-    return gulp.src('./src/js/**/*.js')
-        .pipe(concat('script.js'))
+    return gulp.src([
+        './vendor/bower_components/jquery/dist/jquery.js',
+        './src/js/**/*.js'
+    ]).pipe(concat('script.js'))
         .pipe(gulp.dest('./web/js'));
 });
 
-gulp.task('uglify', function () {
+gulp.task('uglify', ['concat'], function () {
     return gulp.src('./web/js/script.js')
         .pipe(uglify())
         .pipe(rename('script.min.js'))
